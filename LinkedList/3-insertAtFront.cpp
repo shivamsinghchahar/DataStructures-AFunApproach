6cd8825c56ae22c156ffa6c3e75ef06c9cbeea77
Shivam Chahar <chahar.shivam@yahoo.com>
2018-08-10 10:59:40 +0530

    Update

--- LinkedList/3-insertAtFront.cpp
+++ LinkedList/3-insertAtFront.cpp
@@ -1,1 +1,46 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+struct Node {
+  int data;
+  Node *next;
+};
+
+void insertAtFront(Node **head, int data) {
+  Node *newNode, *current_node;
+  
+  newNode = (Node*)calloc(1, sizeof(Node));
+  
+  newNode -> data = data;
+  newNode -> next = *head;
+  
+  *head = newNode;
+  
+  current_node = *head;
+  
+  while(current_node != NULL) {
+    cout << current_node -> data << endl;
+    current_node = current_node -> next;
+  }
+}
+
+int main() {
+  Node *head, *second, *end;
+  
+  head = (Node*)calloc(1, sizeof(Node));
+  second = (Node*)calloc(1, sizeof(Node));
+  end = (Node*)calloc(1, sizeof(Node));
+
+  head -> data = 1;
+  head -> next = second;
+  
+  second -> data = 2;
+  second -> next = end;
+  
+  end -> data = 3;
+  end -> next = NULL;
+  
+  insertAtFront(&head, 0);
+  
+}
