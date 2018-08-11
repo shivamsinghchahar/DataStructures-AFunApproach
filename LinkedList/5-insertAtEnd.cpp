dc3b79a3fff68c89b56c5bf3603af90ba26f7cad
Shivam Chahar <chahar.shivam@yahoo.com>
2018-08-11 17:37:24 +0530

    Add Code

--- LinkedList/5-insertAtEnd.cpp
+++ LinkedList/5-insertAtEnd.cpp
@@ -1,1 +1,54 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node *next;
+};
+
+void insertAtEnd(Node **head, int data) {
+	Node *current_node, *newNode;
+	
+	newNode = new Node();
+	
+	newNode -> data = data;
+	newNode -> next = NULL;
+	
+	current_node = *head;
+	
+	while(current_node->next != NULL)
+	       current_node = current_node -> next;
+	       
+   current_node -> next = newNode;
+   current_node = *head;
+   
+   while(current_node != NULL) {
+        cout << current_node -> data << endl;
+        current_node = current_node -> next;
+   }
+	       
+}
+
+int main()
+{
+	Node *head, *second, *end;
+  
+	head = new Node();
+	second = new Node();
+	end = new Node();
+
+	head -> data = 1;
+	head -> next = second;
+
+	second -> data = 2;
+	second -> next = end;
+
+	end -> data = 3;
+	end -> next = NULL;
+
+	insertAtEnd(&head, 25);
+
+	return 0;
+}
