07bac5c23e86e9ddb4a1fe4a54a11cec30a8dd02
chaharshivam <chahar.shivam@yahoo.com>
2018-08-12 12:24:26 +0530

    Add 1-typeDeclaration for Doubly linked list

---
+++ LinkedList/DoublyLinkedList/1-typeDeclaration.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+	In doubly linked list, we can move in two directions i.e. Forward(using *next) and
+	backward(using *prev). Doubly linked list has some adavantages over the singly linked
+	list e.g. deletion requires us to keep track of previous node in singly linked list but
+	in doubly linked list we already have the pointer to previous node.
+
+	there are some disadvantages too:
+	- Requires extra space for *prev pointer
+	- More pointer operations while inserting or deleting a node
+*/
+
+struct DNode
+{
+	int data;
+	Node *prev;
+	Node *next;
+};
