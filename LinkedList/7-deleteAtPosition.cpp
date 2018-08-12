1403def8251b4785c58e49bc34cc7f650527525a
Shivam Chahar <chahar.shivam@yahoo.com>
2018-08-12 11:45:12 +0530

    Update comments

--- LinkedList/7-deleteAtPosition.cpp
+++ LinkedList/7-deleteAtPosition.cpp
@@ -17,40 +17,56 @@ void traverse(Node *head) {
 	}
 }
 
+/*
+	To delete node at any position we have to keep track of current node and previous node:
+	
+	- create prev_node and temp_node
+	- make the prev_node point head and keep the temp_node just ahead of it to keep track of current node
+	- Once the position is reached, connect the next of prev_node to next of temp_node
+	- delete the temp_node
+	
+	We have successfully deleted our node :)
+*/
+
+
 void deleteAtPosition(Node **head, int position) {
 	Node *prev_node, *temp_node;
 
+// 	to keep track of previous node
 	prev_node = *head;
+
+//	 to keep track of current node, always keep it ahead of prev_node
 	temp_node = prev_node -> next;
 
+// 	if we have to delete the head of our list
 	if(position == 1) {
 		traverse(*head);
-		
+// 		Move the head pointer to the next node
 		*head = prev_node -> next;
-		
+// 		Delete the current node
 		delete prev_node;
 		
 		traverse(*head);
 		return;
-
 	}
 
 	int i = 2;
 
 	while(i != position) {
+// 		Move prev_node ahead
 		prev_node = temp_node;
+// 		Move temp_node ahead
 		temp_node = temp_node -> next;
 		i++;
 	}
-	traverse(*head);
 	
+	traverse(*head);
+// 	connect the prev_node to the node next to temp_node
 	prev_node -> next = temp_node -> next;
 
-	if(temp_node)
-		delete temp_node;
+	delete temp_node;
 	
 	traverse(*head);
-
 }
 
 int main()
