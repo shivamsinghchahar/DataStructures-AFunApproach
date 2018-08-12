fc780b229aa9c9cef1932f789cbf1ed5fd46dd7c
chaharshivam <chahar.shivam@yahoo.com>
2018-08-12 20:10:06 +0530

    Update Comments 2-deleteAtPosition.cpp

--- LinkedList/DoublyLinkedList/Deletion/2-deleteAtPosition.cpp
+++ LinkedList/DoublyLinkedList/Deletion/2-deleteAtPosition.cpp
@@ -20,7 +20,16 @@ void traverse(Node *head) {
 	cout << endl;
 }
 /*
-	
+	To delete a node at any position we have to keep track of node that comes before the 
+	position. Follow the below steps:
+
+	- Make a temporary node and previous node
+	- Make the previous node point to head and keep the temporary node ahead of it
+	- Traverse until the position is reached
+	- Connect the *next of previous node with the node ahead of temporary node
+	- Connect the *prev of node that is ahead of temporary node with the previous node
+	- Delete the temporary node
+
 
 */
 
@@ -28,22 +37,28 @@ void deleteAtPosition(Node **head, int position) {
 	Node *tempNode, *prevNode;
 
 	traverse(*head);
+//  Make the prevNode point to head
 	prevNode = *head;
+//  Keep the tempNode ahead of prevNode
 	tempNode = prevNode -> next;
-
+//  Keep track of tempNode's position
 	int i = 2;
+//  Traverse until the position is reached
 	while(i < position) {
 		i++;
-
+	//  Move prevNode ahead
 		prevNode = tempNode;
+	//  Move tempNode ahead
 		tempNode = tempNode -> next;
 	}
-
+//  Make the prevNode point to node ahead of tempNode
 	prevNode -> next = tempNode -> next;
+//  Make the node that is ahead of tempNode point to prevNode	
 	(tempNode -> next) -> prev = prevNode;
+//  Dispose tempNode
+	delete tempNode;
 
 	traverse(*head);
-
 }
 
 int main()
