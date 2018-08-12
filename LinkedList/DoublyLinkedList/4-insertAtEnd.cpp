915c0c60e695d16db86c73efbd6438406b87efa5
chaharshivam <chahar.shivam@yahoo.com>
2018-08-12 17:45:07 +0530

    Update 4-insertAtEnd.cpp

--- LinkedList/DoublyLinkedList/4-insertAtEnd.cpp
+++ LinkedList/DoublyLinkedList/4-insertAtEnd.cpp
@@ -20,24 +20,34 @@ void traverse(Node *head) {
 	cout << endl;
 }
 /*
-	
+	This process is quite easy, we only need to go to tail node and change the *next
+	of current tail node and *prev of new node. Follow the steps below:
+
+	- Make newNode and current_node
+	- Make the *next of newNode NULL
+	- Go to the tail node
+	- Make the *prev of newNode point current_node
+	- Make the *next of current_node point newNode
+
+	Done! :)
 */
 
 void insertAtEnd(Node **head, int data) {
 	Node *current_node, *newNode;
 
 	traverse(*head);
-
+//  Keep track of current node
 	current_node = *head;
-
+//  Initialize new node
 	newNode = new Node();
 	newNode -> data = data;
 	newNode -> next = NULL;
-
+//  Go to tail node
 	while(current_node -> next != NULL)
 		current_node = current_node -> next;
-
+//  Make *prev of newNode point to current_node
 	newNode -> prev = current_node;
+//  Make *next of current_node point to newNode
 	current_node -> next = newNode;
 
 	traverse(*head);
