386689c720a88ddef4a16d8ccc8a0d463a2ad9be
chaharshivam <chahar.shivam@yahoo.com>
2018-08-13 18:36:06 +0530

    Update 3-insertAtEnd.cpp for circular linked list

--- LinkedList/CircularLinkedList/3-insertAtEnd.cpp
+++ LinkedList/CircularLinkedList/3-insertAtEnd.cpp
@@ -21,22 +21,32 @@ void traverse(Node *head) {
 }
 
 /*
-	
+	To insert the node at end, we need to go to the tail node and connect our tail
+	node to new node. Also, we need to connect the new node to head. Follow below 
+	Steps:
+
+	- Make a newNode and currentNode
+	- Traverse until the end is reached
+	- Connect the newNode to head
+	- Connect the end node to newNode
+
 */
 
 void insertAtEnd(Node **head, int data) {
 	Node *newNode, *currentNode;
 
 	traverse(*head);
-
+//  Initialize newNode
 	newNode = new Node();
 	newNode -> data = data;
+//  Keep track of current node
 	currentNode = *head;
-
+//  Go to the end node
 	while(currentNode -> next != (*head))
 		currentNode = currentNode -> next;
-
+//  Connect the newNode to head
 	newNode -> next = *head;
+//  Connect the end node to newNode	
 	currentNode -> next = newNode;
 
 	traverse(*head);
