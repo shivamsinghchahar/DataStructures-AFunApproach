cc07171263dc8c5260ff6b706fc775a4baa44d85
chaharshivam <chahar.shivam@yahoo.com>
2018-08-16 18:12:31 +0530

    Update comments

--- Stacks/LinkedListImplementation.cpp
+++ Stacks/LinkedListImplementation.cpp
@@ -1,23 +1,27 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+/*
+	Let's implement the stack using a linked list. In this approach a new node is added at
+	the front and removed from the front in case of push and pop respectively.
+*/
 struct Stack
 {
 	int data;
 	Stack *next;
 };
-
+// To create stack, returns a Stack node with NULL values
 Stack *createStack() {
 	return NULL;
 }
 
 void push(Stack **top, int data) {
 	Stack *temp = new Stack();
-
+	// Put data into the stack
 	temp -> data = data;
+	// Point to the current top
 	temp -> next = *top;
-
+	// Move the top pointer to temp
 	*top = temp;
 }
 
@@ -26,13 +30,15 @@ int isEmpty(Stack *top) {
 }
 
 int pop(Stack **top) {
+	// To catch the value of data
 	int data;
 	Stack *temp;
-
+    // To keep track of top
 	temp = *top;
+	// Move the top pointer to next element
 	*top = (*top) -> next;
 	data = temp -> data;
-
+    // Dispose the temp
 	delete temp;
 	return data;
 }
