4e030978bfb5a18ea618f5e5feee74770b3edf37
chaharshivam <chahar.shivam@yahoo.com>
2018-08-25 23:59:01 +0530

    Update circularArrayImplementation.cpp

--- Queues/circularArrayImplementation.cpp
+++ Queues/circularArrayImplementation.cpp
@@ -8,21 +8,27 @@ struct Queue {
 	int *array;
 };
 
+
+/*Create a queue of given size*/
 Queue *createQueue(int size) {
 	Queue *Q = new Queue();
 
 	Q -> size = size;
+	// Make the front and rear -1 to tell that the queue is empty
 	Q -> front = Q -> rear = -1;
+	// Make an array with size
 	Q -> array = new int[size];
 
 	return Q;
 }
 
 bool isEmpty(Queue *Q) {
+	// If front is -1 it means queue is empty
 	return (Q -> front == -1);
 }
 
 bool isFull(Queue *Q) {
+	// If rear and front are the same it means that queue is full
 	return ((Q -> rear + 1) % Q -> size == Q -> front);
 }
 
@@ -31,20 +37,22 @@ int Size(Queue *Q) {
 }
 
 void EnQueue(Queue *Q, int data) {
-	
+	// Add the element at the end of queue
 	Q -> rear = (Q -> rear + 1) % Q -> size;
+	// Put data into the array 
 	Q -> array[Q -> rear] = data;
-	
+	// If queue was empty earlier, now it contains only one element. So, front and rear are equal
 	if(Q -> front == -1) Q -> front = Q -> rear;
-
 }
 
 int DeQueue(Queue *Q) {
 	int data = 0;
 
 	data = Q -> array[Q -> front];
+	// If Queue had only one element
 	if(Q -> front == Q -> rear)
 		Q -> front = Q -> rear = -1;
+	// else Increment front
 	else	Q -> front = (Q -> front + 1) % Q -> size;
 
 	return data;
